9ad67338a2553cadc9dad6477a1d32bb1517e488
k2supra <andrijkorolevic@gmail.com>
2022-10-29 13:24:41 +0300

    Created new lab3

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 try:
     print("Введіть символ для пошуку")
-    sym = int(input("->"))
+    sym = input("->")
 
     txt = "gsgtgBGGV *4824*+ rf r/757df 7g5 drnywtr wq%*^^#EThb^E&#E"
-    def symbols(tt):
-        counter = 0
-        for symbols in txt:
-            if sym.isdigit():
-                counter += 1
 
-        print(f"Nums = {counter}, letters = {len(txt) - counter}")
+    exist = sym in txt
+    print(exist)
 
-    def main():
-        slova(txt)
-
-    main()
 
 except Exception as ex:
     print(f"Error", {ex})
