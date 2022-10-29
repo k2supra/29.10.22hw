f73b8c6c4e2359022eec7bc89be73529db7c12e1
k2supra <andrijkorolevic@gmail.com>
2022-10-29 12:56:24 +0300

    Created new lab1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("kolg")
+try:
+
+    text = int(input("->"))
+    def tring(text):
+        return text[::-1]
+    print()
+
+    def main():
+        tring(text)
+
+    main()
+
+
+except Exception as ex:
+    print(f'Error', {ex})
