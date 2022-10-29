6d343cd8943fb3c5fd13fbe263ef32df4ab80542
k2supra <andrijkorolevic@gmail.com>
2022-10-29 14:14:21 +0300

    Created new lab4

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("kolg")
+try:
+    print("Введіть рядок")
+    txt = input("->")
+    print("Введіть слово для пошуку")
+    sym = input("->")
+    print("Введіть слово для заміни")
+    chn = input("->")
+
+
+    txxt = txt.replace(sym, chn)
+    print(txxt)
+
+except Exception as ex:
+    print(f"Error", {ex})
+
